0f5a9133b00a0df6a179198b65e0f849160b275a
KarenNR <74948680+KarenNR@users.noreply.github.com>
2022-01-04 18:22:48 -0600

    game functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,16 +25,29 @@ export class AppComponent {
   // Keeps track of active game
   activeGame = true;
 
-  changeState(element: any, button: number) {
+  // Name of winner
+  winner = 'Gato';
+
+  // Winning positions
+  winningPositions = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]];
+
+  // Player selected squares
+  select1: number[] = [];
+  select2: number[] = [];
+
+  // Change state of a button when clicked
+  changeState(element: string, button: number) {
     let currentState = '';
     // Set current state of game
     if (this.player1) {
       currentState = 'cross';
-      element.textContent = 'X';
+      document.getElementById(element)!.textContent = 'X';
+      this.select1.push(button);
     }
     else {
       currentState = 'circle';
-      element.textContent = 'O';
+      document.getElementById(element)!.textContent = 'O';
+      this.select2.push(button);
     }
     // Change class of clicked button according to current state
     switch (button) {
@@ -48,7 +61,63 @@ export class AppComponent {
       case 8: this.state8 = currentState; break;
       case 9: this.state9 = currentState; break;
     }
+    // Check if game is won
+    this.checkWon();
     // Change player turn
     this.player1 = !this.player1;
+  }
+
+  // Check if game is won
+  checkWon() {
+    // If all buttons are clicked
+    if (this.state1 !== '' && this.state2 !== '' && this.state3 !== '' && this.state4 !== '' && this.state5 !== '' && this.state6 !== '' && this.state7 !== '' && this.state8 !== '' && this.state9 !== '') {
+      this.activeGame = false;
+    }
+    // Check states of buttons against winning positions for winner
+    for (let combination of this.winningPositions) {
+      // If player 1 has positions equal to some winning position, player 1 wins
+      if (this.select1.includes(combination[0]) && this.select1.includes(combination[1]) && this.select1.includes(combination[2])) {
+        this.activeGame = false;
+        this.winner = "Jugador 1";
+      }
+      // If player 2 has positions equal to some winning position, player 2 wins
+      else if (this.select2.includes(combination[0]) && this.select2.includes(combination[1]) && this.select2.includes(combination[2])) {
+        this.activeGame = false;
+        this.winner = "Jugador 2";
+      }
+    }
+  }
+
+  // Reset Game
+  resetGame() {
+    // Reset turn
+    this.player1 = true;
+    // Reset active game
+    this.activeGame = true;
+    // Reset text contents of each button
+    document.getElementById('button1')!.textContent = '';
+    document.getElementById('button2')!.textContent = '';
+    document.getElementById('button3')!.textContent = '';
+    document.getElementById('button4')!.textContent = '';
+    document.getElementById('button5')!.textContent = '';
+    document.getElementById('button6')!.textContent = '';
+    document.getElementById('button7')!.textContent = '';
+    document.getElementById('button8')!.textContent = '';
+    document.getElementById('button9')!.textContent = '';
+    // Reset states of each button
+    this.state1 = '';
+    this.state2 = '';
+    this.state3 = '';
+    this.state4 = '';
+    this.state5 = '';
+    this.state6 = '';
+    this.state7 = '';
+    this.state8 = '';
+    this.state9 = '';
+    // Reset winner
+    this.winner = "Gato";
+    // Reset selected squares
+    this.select1 = [];
+    this.select2 = [];
   }
 }
